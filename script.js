2d936ded61b5a94873a9cf22d4a66a240dfef999
odd3ye <103198048+odd3ye@users.noreply.github.com>
2022-08-05 20:14:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let users = [
+    {
+        id: 0,
+        photo: "",
+        username: "odd3ye",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum possimus voluptatibus veritatis doloremque at officiis cum deserunt, maxime minima dolore velit ducimus dicta?",
+        fields: ["Editor"],
+        edits: [
+            //EDIIIIIITS
+            "lWbGgPLhCDI",
+            "1rgpk_OqfVA",
+            "uue0nCuTjoI"
+        ]
+    },
+]
+
+function header(data) {
+    document.getElementById("header").innerHTML = data.username + ".zip";
+};
+
+function profile(data) {
+    const { id, photo, username, bio, fields } = data;
+    //PROFILE PHOTO
+    document.getElementById("profile-photo").src = photo;
+    //username
+    document.getElementById("username").innerHTML = username;
+    //bio
+    document.getElementById("bio").innerHTML = bio;
+    //fields
+    fields.map(function (val, index) {
+        document.getElementById("fields").innerHTML += `<span class="text-muted">${val}</span>`;
+    })
+};
+
+function content(data) {
+    let edits = data.edits;
+    edits.map(function (val) {
+        document.getElementById("content").innerHTML += `
+                <div class="col-md-4 col-sm-6 col-12 my-2">
+                <a href="https://www.youtube.com/watch?v=${val}" >
+                        <img src="https://img.youtube.com/vi/${val}/0.jpg" style="width: 100%; border: 1.5px solid black;" class="bg-white p-3">
+                        </a>
+                </div>`
+    })
+}
+header(users[0])
+profile(users[0]);
+content(users[0]);
